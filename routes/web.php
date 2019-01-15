6e264e92fd01edcace7d2dbfb700d36af3f03e43
Tupay <lutfi@mhs.ubharajaya.ac.id>
2019-01-15 19:59:00 +0700

    [Story 14] memisahkan api saat register

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,6 @@ $router->get('/all', 'MailController@index');
 // $router->group(['middleware' => ['token_validate'],'prefix' => '/'], function() use ($router){
 	$router->post('/send', 'MailController@send');
 	$router->post('/push', 'PushNotif@index');
+
+	$router->post('/send-email-activation', 'ActivationEmail@index');
 // });
