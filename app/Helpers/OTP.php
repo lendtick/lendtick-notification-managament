b4865538295c9b1a67df7ca8a44aa138831402bf
Tupay <lutfi@mhs.ubharajaya.ac.id>
2019-02-08 17:45:29 +0700

    enhance fixing sms awo

--- app/Helpers/OTP.php
+++ app/Helpers/OTP.php
@@ -1,5 +1,7 @@
 <?php 
+
 namespace App\Helpers;
+use App\Helpers\RestCurl;
 
 class OTP
 {
@@ -10,5 +12,35 @@ class OTP
 		} catch (Exception $e) {
 			return 1111;
 		}
+	}
+
+	// 
+	public static function OTPsend($phone_number= null , $message = null)
+	{
+		try {
+		
+		$user_awo           = env('AWO_USER');
+		$password_user_awo  = env('AWO_PASSWORD');
+		$sender             = env('AWO_SENDER');  
+
+		$data = array(
+			'user'		=> $user_awo,
+			'pwd'		=> $password_user_awo,
+			'sender'	=> $sender,
+			'msisdn'	=> $phone_number,
+			'message'	=> $message,
+		);
+
+		// return env('AWO_URL_SEND_OTP').'?user='.$user_awo.'&pwd='.$password_user_awo.'&sender='.$sender.'&msisdn='.$phone_number.'&message='.$message;
+		$url = env('AWO_URL_SEND_OTP').'?user='.$user_awo.'&pwd='.$password_user_awo.'&sender='.$sender.'&msisdn='.$phone_number.'&message='.$message;
+
+		$res = RestCurl::exec('GET',$url,array(),'');
+
+		return $res;
+
+		} catch (Exception $e) {
+			return false;
+		}
+
 	}
 }
