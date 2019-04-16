1d028238709a6ec80bed8c8f5db958db0b8464fc
Tupay <lutfi@mhs.ubharajaya.ac.id>
2019-04-16 17:53:42 +0700

    add API credit 2

--- app/Http/Controllers/EmailNotifLoan.php
+++ app/Http/Controllers/EmailNotifLoan.php
@@ -108,32 +108,70 @@ class EmailNotifLoan extends Controller
 			if(empty($request->json())) throw New \Exception('Params not found', 500);
 
 			$this->validate($request, [
-				'email_credit_two'	=> 'required|email',
-				'loan_number'=>'required'
+				'email_credit_two'	=> 	'required|email',
+				'loan_number'		=>	'required',
+				'name_customer'		=>	'required',
+				'id_customer'		=>	'required',
+				'status'			=>  'required'
 			]);   
 
+			if ($request->status == 'reject') {
+
+				$res = TemplateEmail::get(
+					env('URL_HTML_FAILED_LOAN_BY_CREDIT_2'),
+					array(
+						'USER' 			=> $request->name_customer,
+						'LOAN_NUMBER' 	=> $request->loan_number,
+					)
+				);
+				$data = [
+					'subject' => 'Approval Pinjaman Kredit 2 - Koperasi Astra | loan number '.$request->loan_number,
+					'body' => $res,
+					'to' => $request->email_credit_two,
+					'send_date' => date('Y-m-d H:i:s')
+				];
+				$send = Mail::to($request->email_credit_two)->send(new SendEmail($data));
+
+				$body = array(
+					'title' => 'Koperasi Astra', 
+					'body' 	=> 'Pinjaman kamu ditolak'
+				);
+
+				$res = FCM::individu($request->id_customer , $body); 
+
+			} elseif ($request->status == 'approve') {
+
+				$res = TemplateEmail::get(
+					env('URL_HTML_APPROVE_LOAN_BY_CREDIT_2'),
+					array(
+						'USER' 			=> $request->name_customer,
+						'LOAN_NUMBER' 	=> $request->loan_number,
+					)
+				);
+				$data = [
+					'subject' => 'Approval Pinjaman Kredit 2 - Koperasi Astra | loan number '.$request->loan_number,
+					'body' => $res,
+					'to' => $request->email_credit_two,
+					'send_date' => date('Y-m-d H:i:s')
+				];
+				$send = Mail::to($request->email_credit_two)->send(new SendEmail($data));
+
+				$body = array(
+					'title' => 'Koperasi Astra', 
+					'body' 	=> 'Pinjaman kamu disetujui'
+				);
+
+				$res = FCM::individu($request->id_customer , $body); 
+				
+			} else {
+				throw New \Exception('Status Undefined', 500);
+			}
 			
-			// $res = TemplateEmail::get(
-			// 	env('URL_HTML_REJECT_APPROVAL_ADMIN'),
-			// 	array(
-			// 		'NAME' => $request->name_customer,
-			// 		'EMAIL' => $request->email_customer
-			// 	)
-			// );
-			$data = [
-				'subject' => 'Approval Pinjaman Kredit 2 - Koperasi Astra',
-				'body' => "Email to credit 2 | loan number ".$request->loan_number ,
-				'to' => $request->email_credit_two,
-				'send_date' => date('Y-m-d H:i:s')
-			];
-			$send = Mail::to($request->email_credit_two)->send(new SendEmail($data));
-
-
 			$this->notifRepo->create($data);
 			$status   = 1;
 			$httpcode = 200;
-			$data     = 'Berhasil Kirim';  
-			$errorMsg = null;
+			$data     = '';  
+			$errorMsg = 'Berhasil Kirim';
 
 		}catch(\Exception $e){
 			$status   = 0;
