774c7c655772225db4bdd3e0529fd41929d3410b
Tupay <lutfi@mhs.ubharajaya.ac.id>
2020-07-05 12:41:34 +0700

    fxixng error ssl

--- app/Helpers/TemplateEmail.php
+++ app/Helpers/TemplateEmail.php
@@ -5,8 +5,15 @@ namespace App\Helpers;
 class TemplateEmail
 {
 	public static function get($templateName, $variables) {
-		$template = file_get_contents($templateName);
+		$arrContextOptions=array(
+			"ssl"=>array(
+				"verify_peer"=>false,
+				"verify_peer_name"=>false,
+			),
+		);  
 
+		$template = file_get_contents($templateName, false, stream_context_create($arrContextOptions));
+		
 		foreach($variables as $key => $value)
 		{
 			$template = str_replace('{{ '.$key.' }}', $value, $template);
